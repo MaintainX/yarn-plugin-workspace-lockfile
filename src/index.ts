3660e416b624eef48b212d52c694b5fc3236a5c6
Michael Ferris <micfer@getmaintainx.com>
2025-06-23 15:28:11 -0400

    Create workflow to autofix dependabot PR

--- src/index.ts
+++ src/index.ts
@@ -294,7 +294,8 @@ ${lockfilePackages}`;
     // Write the workspace lockfile
     const workspaceLockfilePath = ppath.join(workspace.cwd, "yarn.workspace.lock");
 
-    if (immutable) {
+    const isImmutable = process.env.YARN_WORKSPACE_LOCKFILE_FORCE_WRITE !== "true" && immutable;
+    if (isImmutable) {
       const existingLockfile = await xfs.readFilePromise(workspaceLockfilePath, "utf-8");
       if (existingLockfile === lockfileContent) {
         return;
